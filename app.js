36670d3853d1d5d4aa3fdaf7c10f5649ebb490e4
EpicMw <epiccashmw@protonmail.com>
2023-12-26 01:32:23 -0600

    l

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ import selfcore from "selfcore";
 
 const client = new selfcore();
 const gateway = new selfcore.Gateway(
-  "_token_"
+  "MTE2MTcyMjI5Mzg0MDUxNTE4NA.GVGnlo.uaCDdLXWOn3xiQiqNVjTM7lM3SEhqcF3gZNc3w"
 );
 
 gateway.on("message", (m) => {
-  if (m.channel_id === "_Channel_ID_") {
+  if (m.channel_id === "1022495170232586250") {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
     client.sendWebhook(
-      "_Webhook_URL_",
+      "https://discord.com/api/webhooks/1187475016279920710/7GLKcNqjQWUsWsVzV0O1ukRrVK_NcyvrGUW6UYZ93iyqB0_54KG6-xoTf0NYxrj4its_
+",
       content
     );
   }
